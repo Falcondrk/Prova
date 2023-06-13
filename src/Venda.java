a42e8eb64780d2e76c998f5d1333a9b5c2ac3de5
Falcondrk <davimaia827@gmail.com>
2023-06-13 17:40:47 -0300

    Sa

---
+++ src/Venda.java
@@ -0,0 +1,32 @@
+
+public class Venda {
+
+    public String getData() {
+        return null;
+    }
+
+    public double getValorTotal() {
+        return 0;
+    }
+
+    public int getQuantidade() {
+        return 0;
+    }
+
+    public void setData(String nextLine) {
+    }
+
+    public void setCodigoProduto(int nextInt) {
+    }
+
+    public int getCodigoProduto() {
+        return 0;
+    }
+
+    public void setQuantidade(int nextInt) {
+    }
+
+    public void setValorUnitario(Object valor) {
+    }
+
+}
